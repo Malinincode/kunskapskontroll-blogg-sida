efee47ee9976c871d53fdcb73caa5386aaf973d8
Malin Volle <malin.volle@gmail.com>
2022-05-20 19:45:42 +0200

    start efter koppling

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ echo "</pre>";
 
 
 $message = "";
+
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -16,9 +19,9 @@ $message = "";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin</title>
 </head>
 <body>
-    
+
 </body>
 </html>
